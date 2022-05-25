a24960c0754f059d69a49cc82f48426d80619ecf
Rahul Shah <iq.nit.student@gmail.com>
2022-05-25 23:31:59 +0530

    VIDEO FIX

--- lib/model/feedModel.dart
+++ lib/model/feedModel.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/foundation.dart';
 
+enum UrlType { IMAGE, VIDEO, UNKNOWN }
+
 class FeedModel {
   final String userName;
   final String userImage;
   final String feedDuration;
   final String feedMedia;
   final String uid;
+  final UrlType feedType;
 
-  FeedModel({
-    required this.userName,
-    required this.userImage,
-    required this.feedDuration,
-    required this.feedMedia,
-    required this.uid,
-  });
+  FeedModel(
+      {required this.userName,
+      required this.userImage,
+      required this.feedDuration,
+      required this.feedMedia,
+      required this.uid,
+      required this.feedType});
   factory FeedModel.fromDB(Map<String, dynamic> data) {
+    var mediaUrl = data['feed_media'] as String;
     return FeedModel(
         userName: data['feed_user_name'],
         userImage: data['feed_user_image'],
         feedDuration: data['feed_duration'],
         feedMedia: data['feed_media'],
+        feedType: mediaUrl == ""
+            ? UrlType.UNKNOWN
+            : mediaUrl.contains(".mp4")
+                ? UrlType.VIDEO
+                : UrlType.IMAGE,
         uid: data['feed_user_register_id']);
   }
 }
--- lib/widgets/postConatiner.dart
+++ lib/widgets/postConatiner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:softa/model/feedModel.dart';
+import 'package:softa/widgets/videoWidget.dart';
 
 class PostConatainer extends StatelessWidget {
   final FeedModel data;
@@ -17,15 +18,16 @@ class PostConatainer extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
           color: Theme.of(context).cardColor),
       child: Stack(children: [
-        data.feedMedia != ""
-            ? Container(
-                height: 400,
-                width: MediaQuery.of(context).size.width,
-                child: Image(
-                  image: NetworkImage(data.feedMedia.split(',').first),
-                  fit: BoxFit.cover,
-                ))
-            : Container(),
+        data.feedType == UrlType.UNKNOWN
+            ? Container()
+            : data.feedType == UrlType.IMAGE
+                ? Container(
+                  height: 400,
+                  width: double.infinity,
+                  child: Image.network(data.feedMedia.split(',').first,
+                      fit: BoxFit.fill),
+                )
+                : VideoWidget(url: data.feedMedia),
         Positioned(
           top: 0,
           right: 10,
